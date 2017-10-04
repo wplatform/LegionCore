b086c29f950e303380cd83cc56752e05a2370202
Gildor <gildor55@gmail.com>
2017-10-04 21:37:05 +0200

    Core/Spells: EffectSanctuary should stop attack (#20492)
    (cherry picked from commit 82e58a0c6fab207ffa29045cdd8c52be38de9d63)

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -3650,6 +3650,9 @@ void Spell::EffectSanctuary(SpellEffIndex /*effIndex*/)
     if (!unitTarget)
         return;
 
+    if (unitTarget->GetTypeId() == TYPEID_PLAYER)
+        unitTarget->ToPlayer()->SendAttackSwingCancelAttack();     // melee and ranged forced attack cancel
+
     unitTarget->getHostileRefManager().UpdateVisibility();
 
     Unit::AttackerSet const& attackers = unitTarget->getAttackers();
